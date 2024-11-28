8c5cc1e69fc67dbf79ca559b14d4da3724ee6dc5
B1NUR4 <binuraofficial32@gmail.com>
2024-11-28 18:00:40 +0530

    appbar design was done

--- lib/main.dart
+++ lib/main.dart
@@ -10,47 +10,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Space App",
       debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
           backgroundColor: Colors.black,
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                height: 135,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 255, 230, 0),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              Container(
-                height: 135,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 255, 230, 0),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              Container(
-                height: 135,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 255, 230, 0),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              Container(
-                height: 230,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 255, 230, 0),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              )
-            ],
+          shadowColor: Colors.yellow,
+          elevation: 4.0,
+          title: const Text(
+            "BLACK HOLE",
+            style: TextStyle(
+                fontWeight: FontWeight.w700, fontSize: 23, color: Colors.white),
           ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.menu, color: Colors.white,),
+            ),
+          ],
         ),
       ),
     );
